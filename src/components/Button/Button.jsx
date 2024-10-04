236b00029a1a131852f154f5052a1d62a254da36
OlgaKozoriz07 <161890591+OlgaKozoriz07@users.noreply.github.com>
2024-10-04 11:43:43 +0200

    markup

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,12 @@
+const Button = ({ btnType = "button", handleClick, children }) => {
+  return (
+    <button
+      onClick={() => handleClick(children.toLowerCase())}
+      type={btnType}
+      aria-label={`button ${children}`}
+    >
+      {children}
+    </button>
+  );
+};
+export default Button;
